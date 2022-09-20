0399035bcda77e0b82da1f935a812816abf1c943
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-20 13:11:08 +0300

    saakums1

---
+++ money.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+
+
+class Purse:
+        def__init__(self,valuta, name='Unknown'):
+                self.money=0.00
+                self.valuta=valuta
+                self.name=name
+
+        
+
